afc121b90cbf07535c043d55cf28d3d45881e873
dannyradden <dradden@gmail.com>
2017-09-22 03:07:20 -0600

    starting without ant client stuff

--- client/src/containers/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (
-  <div className="app-container">
-    <h1>I am the 404 page</h1>
-  </div>
-);
-
-export default NotFound;
--- client/src/redux/constants/googleMap.js
+++
@@ -1,2 +0,0 @@
-export const OPEN_MAP = 'OPEN_MAP';
-export const CLOSE_MAP = 'CLOSE_MAP';
